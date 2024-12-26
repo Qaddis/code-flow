8a0d391858831519939e7aab838a4e0d8e9719cc
Qaddis <svyatoslavb1107@gmail.com>
2024-12-26 14:02:18 +0300

    Added Technology section to the main page

---
+++ src/components/pages/MainPage/TechCard.vue
@@ -0,0 +1,83 @@
+<script>
+export default {
+	props: {
+		tech: String
+	}
+}
+</script>
+
+<template>
+	<article class="tech-card">
+		<img
+			:src="`/techs/${tech}.webp`"
+			:alt="tech + ' Banner'"
+			class="tech-card__logo"
+		/>
+		<h4 class="tech-card__title">{{ tech }}</h4>
+	</article>
+</template>
+
+<style scoped lang="scss">
+.tech-card {
+	width: 200px;
+	background-color: var(--main);
+	border-radius: 10px;
+	padding: 10px;
+	position: relative;
+
+	&__logo {
+		width: 100%;
+		margin-bottom: 15px;
+	}
+
+	&__title {
+		font-family: var(--jost);
+		font-size: 1.5rem;
+		font-weight: 600;
+		text-align: center;
+	}
+
+	&::before,
+	&::after {
+		content: "";
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		translate: -50% -50%;
+		width: 102%;
+		height: 102%;
+		background: conic-gradient(
+			from var(--angle),
+			var(--blue) 0%,
+			transparent 10%,
+			var(--green) 40%,
+			transparent 60%,
+			var(--blue) 90%
+		);
+		border-radius: 10px;
+		z-index: -1;
+		animation: spin 3s linear infinite;
+	}
+
+	&::before {
+		filter: blur(10px);
+		opacity: 0.5;
+	}
+}
+
+@property --angle {
+	syntax: "<angle>";
+	initial-value: 0deg;
+	inherits: false;
+}
+
+@keyframes spin {
+	from {
+		--angle: 0deg;
+	}
+
+	to {
+		--angle: 360deg;
+	}
+}
+</style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,8 +1,27 @@
 <script>
 import Landing from "../components/pages/MainPage/Landing.vue"
+import TechCard from "../components/pages/MainPage/TechCard.vue"
 
 export default {
-	components: { Landing }
+	components: { Landing, TechCard },
+	data() {
+		return {
+			techs: [
+				"HTML",
+				"CSS",
+				"SCSS",
+				"Tailwind",
+				"JavaScript",
+				"TypeScript",
+				"Python",
+				"Golang",
+				"React",
+				"Vue",
+				"Next",
+				"Nuxt"
+			]
+		}
+	}
 }
 </script>
 
@@ -10,6 +29,14 @@ export default {
 	<div class="main-page">
 		<div class="wrapper">
 			<landing />
+
+			<section class="technologies">
+				<h3 class="heading">Технологии</h3>
+
+				<div class="container">
+					<tech-card v-for="(tech, index) in techs" :key="index" :tech />
+				</div>
+			</section>
 		</div>
 	</div>
 </template>
@@ -20,10 +47,30 @@ export default {
 	background-color: var(--main);
 	background: radial-gradient(circle at 96vw 8vh, #1b998b65, transparent 40%),
 		radial-gradient(circle at 4vw 92vh, #7494ea65, transparent 40%);
+	background-attachment: fixed;
 }
 
 .wrapper {
 	max-width: 1140px;
 	margin: 0 auto;
+}
+
+.heading {
+	font-family: var(--jost);
+	font-weight: bold;
+	font-size: 2rem;
+	text-align: center;
+}
+
+.technologies {
+	margin: 100px 0 0;
+
+	.container {
+		margin-top: 25px;
+		display: grid;
+		justify-content: center;
+		grid-template-columns: repeat(4, 200px);
+		gap: 25px;
+	}
 }
 </style>
